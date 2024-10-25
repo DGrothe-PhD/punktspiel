0af9a8d7b82603cd30606eb0a134ea49a955aeb4
DGrothe-PhD <dag_fs@t-online.de>
2024-10-25 18:24:28 +0200

    initial commit (draft)

---
+++ android/app/src/main/kotlin/com/example/punktspiel/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.punktspiel
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/calc.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+
+class _PointsCalc {
+
+  void addPoints() {
+
+    }
+  }
