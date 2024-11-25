492bb6dff7b2e2c46e124c9ff69295ede6e8fb10
DGrothe-PhD <dag_fs@t-online.de>
2024-11-25 22:14:48 +0100

    moving from markdown to table. Problems tbf: too broad for phone screen aiui

--- lib/table.dart
+++ lib/table.dart
@@ -1,3 +1,4 @@
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:markdown/markdown.dart' as md;
 import 'package:flutter_html/flutter_html.dart';
@@ -6,14 +7,30 @@ import './calc.dart';
 import './locales.dart';
 
 class StyleDecorator {
-  static const textstil = TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold);
+  static const textstil = TextStyle(
+    fontSize: 12.0, color: Colors.black, fontWeight: FontWeight.bold
+  );
+  static const monoStil = TextStyle(
+    fontSize: 12.0, color: Colors.black, fontFamily: "B612 Mono",
+    fontFamilyFallback: <String>["Courier"]
+  );
+  static const boldStil = TextStyle(
+    fontSize: 12.0,  fontFamily: 'B612 Mono',
+    color: Colors.black,
+    fontWeight: FontWeight.bold, // Setzt die fetten Buchstaben
+  );
+
+  static Text typewriter(String input){
+    return Text(input, style: monoStil);
+  }
+
   static String write(String inputText){
     return md.markdownToHtml(inputText);
   }
 
-  static Html viewMd(String inputText){
+  static Html viewMd(String inputText, {bool isMD = true}){
     return Html(
-      data: write(inputText)
+      data: isMD ? write(inputText) : inputText
     );
   }
 }
@@ -45,27 +62,71 @@ class TableExample extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     try {
-      var buffer = StringBuffer();
-      buffer.write("# ${Locales.results[Lang.l]} - ${DateFormat('dd.MM.yyyy').format(now)}\n");
-      //
+      StringBuffer buffer = StringBuffer();
+      String headline = "${Locales.results[Lang.l]} - ${DateFormat('dd.MM.yyyy').format(now)}\n";
+      int maxCounts = Spieler.gruppe.map((x) => x.punkte.length).max;
+
+      StringBuffer playerNames = StringBuffer();
       for(var player in Spieler.gruppe){
         String decoration = (Spieler.whoIsWinning().contains(player)) ? winningDecoration : "";
-        buffer.write("## ${player.name}:$decoration\n - ${player.punkte.enumerateString()}\n");
-        buffer.write(" - ${Locales.pointsTotal[Lang.l]} ${player.sumPoints()}\n\n");
+        playerNames.write(" ${player.name.truncate(10)}$decoration |".padLeft(14, " "));
       }
-      /*
-      String placeholder = "# ${Locales.results[Lang.l]} - ${DateFormat('dd.MM.yyyy').format(now)}\n";
+      playerNames.write("\n");
+
+      for(int j=0;j<maxCounts;j++){
+        for(var player in Spieler.gruppe){
+          if(j >= player.punkte.length){
+            buffer.write(" |".padLeft(14, " "));
+            continue;
+          }
+          buffer.write(" ${player.punkte[j]} |".padLeft(14, " "));
+        //buffer.write("## ${player.name}:$decoration\n - ${player.punkte.enumerateString()}\n");
+        //buffer.write(" - ${Locales.pointsTotal[Lang.l]} ${player.sumPoints()}\n\n");
+        }
+        buffer.write("\n");
+      }
+      // do a `===============`
+      buffer.write("${"=" * 14 * Spieler.gruppe.length}\n");
+
+      // Sum of points
       for(var player in Spieler.gruppe){
-        String decoration = (Spieler.whoIsWinning().contains(player)) ? winningDecoration : "";
-        placeholder += "## ${player.name}:$decoration\n - ${player.punkte.enumerateString()}\n";
-        placeholder += " - ${Locales.pointsTotal[Lang.l]} ${player.sumPoints()}\n\n";
-      }*/
+        buffer.write(" ${player.sumPoints()} |".padLeft(14, " "));
+      }
+      // try SelectableText.rich, onTap
+      // as here https://stackoverflow.com/q/60395983/17677104
+      // or some https://api.flutter.dev/flutter/widgets/RichText-class.html
+      // Scrollable (Windows testable I guess)
+      // https://stackoverflow.com/questions/49617934/how-to-make-text-or-richtext-scrollable-in-flutter
+      
       return Center(
           child: Column(
+            //crossAxisAlignment: CrossAxisAlignment.end,
             children: <Widget>[
               SelectionArea(
-                child: StyleDecorator.viewMd(buffer.toString()),
+                child: 
+                RichText(
+                  //TODO test IRL aka Android Phone
+                  selectionRegistrar: SelectionContainer.maybeOf(context),
+                  textAlign: TextAlign.right,
+                  text: TextSpan(
+                    text: headline,
+                    style: StyleDecorator.monoStil,
+                    children: <TextSpan>[
+                      TextSpan(
+                        text: playerNames.toString(),
+                        style: StyleDecorator.textstil,
+                      ),
+                      TextSpan(
+                        text: buffer.toString(),
+                        style: StyleDecorator.monoStil,
+                      ),
+                    ],
+                  ),
+                ),
+                //child: StyleDecorator.typewriter(buffer.toString()),
+                //child: StyleDecorator.viewMd(buffer.toString(), isMD: true),
               ),
+              const SizedBox(height:20),
             SizedBox(
               width: 120,
               height: 50,
