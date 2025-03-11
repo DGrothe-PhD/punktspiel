8d4e81cc0483ca6ed8b766364bf9d839f415d8d4
DGrothe-PhD <dag_fs@t-online.de>
2025-03-11 16:06:54 +0100

    make settings page stateful, get api call for latest version.

--- lib/settings.dart
+++ lib/settings.dart
@@ -1,6 +1,9 @@
 //tbd
+import 'dart:io';
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:swipe_to/swipe_to.dart';
+import 'package:http/http.dart' as http;
 //import 'package:flutter_html/flutter_html.dart';
 import './locales.dart';
 
@@ -12,19 +15,48 @@ class SettingsAppWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         resizeToAvoidBottomInset : true,//maybe false if keyboard
-        appBar: AppBar(title: Text(Locales.settingsTitle[Lang.l])),
+        //appBar: AppBar(title: Text(Locales.settingsTitle[Lang.l])),
         body: 
         SingleChildScrollView(
-          child: SettingsPage(),
+          child: MySettingsPage(title: Locales.settingsTitle[Lang.l]),
         )
     );
     // reduce to the max. Returning MaterialApp shows black screen...
   }
 }
 
-class SettingsPage extends StatelessWidget {
+class MySettingsPage extends StatefulWidget{
+  const MySettingsPage({super.key, required this.title});
+  final String title;
+  @override
+  MySettingsPageState createState() => MySettingsPageState();
+}
+
+class MySettingsPageState extends State<MySettingsPage> {
   final now = DateTime.now();
-  SettingsPage({super.key});
+  //SettingsPage({super.key});
+  //late String latestAppVersion;
+
+  @override
+  initState(){
+    super.initState();
+  }
+
+  //Network Request to get latest release version of this app.
+  Future<String> fetchLatestAppVersionDetails() async {
+    final response = await http.get(
+      Uri.parse('https://api.github.com/repos/DGrothe-PhD/punktspiel/releases/latest')
+    );
+    if(response.statusCode == 200){
+      Map<String, dynamic> json = jsonDecode(response.body);
+      String tagName = json['tag_name'];
+      String publishedAt = json['published_at'];
+      return "Latest version: $tagName\nPublished at: $publishedAt\n";
+    }
+    else{
+      throw HttpException(Locales.isOffline[Lang.l]);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,7 +67,20 @@ class SettingsPage extends StatelessWidget {
           child: Column(
             children: <Widget>[
             const Text("… coming soon …"),
-            const SizedBox(height:200),
+            const SizedBox(height:177),
+            FutureBuilder<String>(
+              future: fetchLatestAppVersionDetails(),
+              builder: (context, snapshot) {
+                if(snapshot.connectionState == ConnectionState.waiting){
+                  return const CircularProgressIndicator();
+                }
+                if(snapshot.hasError){
+                  return Text("Error: ${snapshot.error}");
+                }
+                return Text("${snapshot.data}");
+              },
+            ),
+            //Text(latestAppVersion),
             SizedBox(
               width: 120,
               //height: 50,
@@ -54,6 +99,9 @@ class SettingsPage extends StatelessWidget {
         ),
       );  
   }
+    on HttpException catch(e){
+      return Text(e.toString());
+    }
     catch (exception){
       //Make exception readable.
       return Text(exception.toString());
